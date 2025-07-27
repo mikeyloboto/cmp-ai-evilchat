0bcf65bea227ba4b655880f4c85f5cfdf24236f1
Evilbas <dimonloboto@gmail.com>
2025-07-27 19:37:23 +0800

    small changes

--- lua/cmp_ai_evilchat/compare.lua
+++ lua/cmp_ai_evilchat/compare.lua
@@ -1,10 +1,10 @@
 return function(entry1, entry2)
-  if entry1.source.name == 'cmp_ai' and entry2.source.name == 'cmp_ai' then
+  if entry1.source.name == 'cmp_ai_evilchat' and entry2.source.name == 'cmp_ai_evilchat' then
     return (#entry1.completion_item.label > #entry2.completion_item.label)
   end
-  if entry1.source.name == 'cmp_ai' and entry2.source.name ~= 'cmp_ai' then
+  if entry1.source.name == 'cmp_ai_evilchat' and entry2.source.name ~= 'cmp_ai_evilchat' then
     return true
-  elseif entry1.source.name ~= 'cmp_ai' and entry2.source.name == 'cmp_ai' then
+  elseif entry1.source.name ~= 'cmp_ai_evilchat' and entry2.source.name == 'cmp_ai_evilchat' then
     return false
   else
     return nil
